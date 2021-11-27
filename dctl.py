6d74c542653a2a26b3dd478d73bb77646d78d39f
sebug <sebastian.gfeller@gmail.com>
2021-11-27 22:03:45 +0100

    Proper formatting for calendar day

--- dctl.py
+++ dctl.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 import sys
 import os
+import requests
 
 Token=os.environ.get('DIONYSIA_TOKEN')
 ApiUrl=os.environ.get('DIONYSIA_API_URL')
 
 def calendar(date):
-    print(Token)
-    print(ApiUrl)
-    print(date)
+    request_url = ApiUrl + '/getCalendar/' + date
+    headers = { "Authorization": "Bearer " + Token }
+    response = requests.get(request_url, headers = headers)
+    structured = response.json()
+    lessons = structured['lessons']
+    print('Your lessons for ' + date)
+    for lesson in lessons:
+        print(lesson['type'])
+        print('From ' + lesson['start'] + ' to ' + lesson['end'])
+        print('At ' + lesson['teren']['name'] + ', ' + lesson['teren']['address'])
+        for coach in lesson['coaches']:
+            print('By ' + coach['name'])
+        print('with')
+        for student in lesson['students']:
+            print(' - ' + student['name'])
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
