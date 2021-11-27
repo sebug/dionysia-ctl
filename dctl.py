115aa17587daec5f1a8d026a57a34f9969dbfc2b
sebug <sebastian.gfeller@gmail.com>
2021-11-27 13:06:15 +0100

    Dispatching to function

--- dctl.py
+++ dctl.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+import sys
+
+def calendar(date):
+    print(date)
 
 if __name__ == '__main__':
-    print("Usage: dctl.py action args")
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'calendar':
+            calendar(sys.argv[2])
+        else:
+            print(sys.argv[1])
+    else:
+        print("Usage: dctl.py action args")
 
